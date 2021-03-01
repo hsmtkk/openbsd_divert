011a53bf89ae464ad7f5d164e08a0c962834c330
foo <foo@bar>
2021-03-01 18:26:01 +0900

    up

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# openbsd_divert
 OpenBSD divert demo
+
+# memo
+
+enable
+
+```
+PIPENV_VENV_IN_PROJECT
+```
+
+environment variable to create .venv directory in your project.
+
--- sample.py
+++ sample.py
@@ -1,10 +1,16 @@
 import socket
 
+import scapy.utils
+
 IPPROTO_DIVERT = 258
+DIVERT_PORT = 700
+PCAP_FILE = 'out.pcap'
 
 with socket.socket(socket.AF_INET, socket.SOCK_RAW, IPPROTO_DIVERT) as sock:
-    sock.bind(('0.0.0.0', 700))
-    while True:
-        data, peer = sock.recvfrom(2048)
-        print("received %d bytes" % len(data))
-        sock.sendto(data, peer)
+    sock.bind(('0.0.0.0', DIVERT_PORT))
+    with scapy.utils.PcapWriter(PCAP_FILE) as writer:
+        while True:
+            data, peer = sock.recvfrom(2048)
+            print("received %d bytes from %s" % (len(data), str(peer)))
+            writer.write(data)
+            sock.sendto(data, peer)
